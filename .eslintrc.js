f81a41f5f70f33957c20c2b5f0f72534f3e41a64
su- <aaaiiiuuu35@gmail.com>
2020-01-18 16:17:50 +0900

    設定ファイル整形

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,44 @@
-module.exports ={
-    'root': true,
-    'env': {
-        "es6": true,
-        "node": true,
-        browser: true,
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    version: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "parser": "babel-eslint",
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "parserOptions": {
-        "version": 2018,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended",
-        "prettier/react",
-    ],
-    "globals": {
-        "window": "readonly",
-        "location": "readonly",
-        'document': 'readonly',
-    },
-    "rules": {
-        "no-console": "warn",
-        "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
-        "semi-spacing": ["error", {"after": true, "before": false}],
-        "semi-style": ["error", "last"],
-        "no-extra-semi": "error",
-        "no-unexpected-multiline": "error",
-        "no-unreachable": "error",
-        "indent": [ 2, 2 ],
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'prettier/react'],
+  globals: {
+    window: 'readonly',
+    location: 'readonly',
+    document: 'readonly',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    indent: [2, 2],
 
-        "no-extra-bind": "warn",
-        "no-fallthrough": "warn",
-        "no-empty": "warn",
-        "block-scoped-var": "warn",
-        "camelcase": "warn",
-        "linebreak-style": "warn",
-        "no-trailing-spaces": "warn",
-        "unicode-bom": "warn",
-
-    },
+    'no-extra-bind': 'warn',
+    'no-fallthrough': 'warn',
+    'no-empty': 'warn',
+    'block-scoped-var': 'warn',
+    camelcase: 'warn',
+    'linebreak-style': 'warn',
+    'no-trailing-spaces': 'warn',
+    'unicode-bom': 'warn',
+  },
 };
