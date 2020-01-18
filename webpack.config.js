896d998416b34ecc65f6f0bd1e4088ca54374aee
su- <aaaiiiuuu35@gmail.com>
2020-01-18 18:27:48 +0900

    Add docker

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 module.exports = {
   output: {
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/front/dist`,
     filename: 'bundle.js?[hash]',
   },
   entry: {
