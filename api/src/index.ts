be94f66653037b48947f4f395be9d3e117c1a533
su- <aaaiiiuuu35@gmail.com>
2020-01-20 00:31:45 +0900

    wisp

---
+++ api/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+import { router } from './routes/users';
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/users', router);
+
+const api = functions.https.onRequest(app);
+export { api };
