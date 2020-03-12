ddfb2f9004b474662755c26cbdec95daf1e77d50
すー <aaaiiiuuu35@gmail.com>
2020-03-12 22:14:17 +0900

    Feat: NavBar sample

--- front/src/Navigator.tsx
+++ front/src/Navigator.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Switch, Route, HashRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
 import GlobalStyle from './components/GlobalStyle';
 import App from './containers/App';
-import NavigationHeader from '@/components/Header/NavigationHeader';
+import ButtonAppBar from '@/components/Header/NavBar';
 
 // eslint-disable-next-line react/prop-types
 export const Router = ({ path = '' }) => {
   return (
     <Switch>
       <Route exact path={`${path}/`} component={App} />
+      <Route exact path={`${path}/test`} component={ButtonAppBar} />
     </Switch>
   );
 };
 
 const Navigator = () => (
-  <>
+  <StylesProvider injectFirst>
     <GlobalStyle />
     <HashRouter>
-      <NavigationHeader />
+      <ButtonAppBar />
       <Router />
     </HashRouter>
-  </>
+  </StylesProvider>
 );
 
 export default Navigator;
