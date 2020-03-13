7f49f0e21641a7e64b27eb7b93c9b9883545ec48
すー <aaaiiiuuu35@gmail.com>
2020-03-13 16:30:08 +0900

    Add: 設定ファイル

---
+++ front/src/firebase/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBGAeRswlpazPRswXmksifHVbM1Z4cR2QM',
+  authDomain: 'webapp-template-01.firebaseapp.com',
+  databaseURL: 'https://webapp-template-01.firebaseio.com',
+  projectId: 'webapp-template-01',
+  storageBucket: 'webapp-template-01.appspot.com',
+  messagingSenderId: '489542780785',
+  appId: '1:489542780785:web:dd1f0bb5a58d34e6bcd8d0',
+  measurementId: 'G-68ZZ3MDD67',
+};
